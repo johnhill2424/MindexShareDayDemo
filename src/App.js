3f6bfc3f2777d8f666e6e5d63ba8ae0b1485e2e2
John Hill <jhill@mindex.com>
2021-06-23 12:05:11 -0400

    updating text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> testing.
+          Mindex Share Day 
         </p>
         <a
           className="App-link"
